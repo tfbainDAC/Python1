432436932fbb557f073545c25f38b74b8af19850
Tracey Bain <t.bain@dundeeandangus.ac.uk>
2019-08-22 16:11:28 +0100

    This is changing the print file

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,1 +1,9 @@
+#
+# Example python file for hello world
+#
 
+def main():                                         #creates function (note no curly braces), uses indentation level to determine
+   print("hello world, how are you")
+
+if __name__ == "__main__":         #checks to see when python module is loaded, if it is executed as a program it will run main()   
+   main()
